df908fc53b24ccf4cd0a84143382afcd89d05892
jorge <jorgebarrera680@gmail.com>
2022-10-31 21:54:22 -0500

    done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import React, { useState } from 'react'
-
+import ColorForm from './ColorForm'
 import ColorBlock from './ColorBlock';
 
 function App(){
